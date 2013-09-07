1f9288439b42324f0562a952372fc9651d8b9d77
Redfrost <contact@simonalex.com>
2013-09-07 23:59:13 +1000

    V1.5 alpha

--- 1.4/assets/js/custom.js
+++ 1.4/assets/js/custom.js
@@ -56,7 +56,7 @@
 //Convert Address into an embedded map
     jQuery(document).ready(function(){
       $(".mapaddress").each(function(){                        
-        var embed ="<iframe width='100%' height='350' frameborder='0' scrolling='no'  marginheight='0' marginwidth='0'   src='https://maps.google.com/maps?&amp;q="+ encodeURIComponent( $(this).text() ) +"&amp;output=embed'></iframe>";
+        var embed ="<iframe width='425' height='350' frameborder='0' scrolling='no'  marginheight='0' marginwidth='0'   src='https://maps.google.com/maps?&amp;q="+ encodeURIComponent( $(this).text() ) +"&amp;output=embed'></iframe>";
           $(this).html(embed);                               
        });
     });
